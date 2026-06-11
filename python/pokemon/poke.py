0ba9f6a9fd107c34d39c23cd448a3852ade6773d
JohananSalomon <ibrahimsalomonve@gmail.com>
2026-06-10 21:29:28 -0300

    pokemon

--- python/pokemon/poke.py
+++ python/pokemon/poke.py
@@ -1,6 +1,8 @@
 import requests
 
-url = "https://pokeapi.co/api/v2/pokemon/pikachu"
+nome = input ("Digite o nome do pókemon:").lstrip().lower() 
+
+url = "https://pokeapi.co/api/v2/pokemon/" + nome 
 
 resposta = requests.get(url)
 
@@ -8,8 +10,18 @@ print ("Status: ",resposta.status_code)
 
 dados = resposta.json()
 
+print("--- informações gerais ---")
 print ("Nome: ",dados['name'])
 print ("Número: ",dados['id'])
 print ("Altura: ",dados['height'])
 print ("Peso: ",dados['weight'])
 
+print ("--- Habilidades ---")
+for item in dados ["types"]:
+    print("-", item ["type"]["name"])
+
+print("--- stats ---")
+for stat in dados ["stats"]:
+    print("-", stat ["stat"]["name"], ":",stat["base_stat"])
+
+atividade função pra mostrar e pra exibir
