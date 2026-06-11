daa64745d5177a5c28b28027b61494f91c86583f
XxDarksidex <ibrahimsalomonve@gmail.com>
2026-06-11 14:22:37 -0300

    jovem_programador

--- python/python-anotação_revisão.py
+++ python/python-anotação_revisão.py
@@ -10,22 +10,6 @@ def adicionar_frutas():
     lista.append ("banana")
     print (lista)    
 
-adicionar_frutas()
-
-;
-
-for i in range(0,10,2):
-    print (i)
-
-;
-
-for i in range(0,10,2):
-    print (i)
-
-;
-
-for i in range(0,10,2):
-    print (i)
 
 
 
